2d72a2c87e3d9c86d06cb06506cc1a4dfb5e3ad5
hanercamilo <hanercamilo@hotmail.com>
2018-06-21 11:26:35 -0500

    Update unnecesarie data

--- js/Utils.js
+++ js/Utils.js
@@ -1,6 +1,2 @@
-sessionStorage.setItem("name", "h.perea");
-sessionStorage.getItem('name');
-sessionStorage.removeItem("name"); 
-
 var jun = moment("2018-06-14T15:00:00Z");
 jun.tz('America/Bogota').format('h');
